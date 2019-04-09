c3234ed1d3f47cdffb142c9c979df453353a850f
opet <opet@RB-LAB207A-001.Academico.edu>
2019-04-08 22:12:05 -0300

    update consulta PessoaDAO

--- src/br/com/opet/model/dao/PessoaDAO.java
+++ src/br/com/opet/model/dao/PessoaDAO.java
@@ -7,12 +7,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import br.com.opet.model.AuxiliarAdministrativo;
+import br.com.opet.model.Enfermeiro;
+import br.com.opet.model.Medico;
 import br.com.opet.model.top.Pessoa;
 
-public class PessoaDAO{
+public class PessoaDAO {
 	public boolean salvar(Pessoa pTMP) {
 		Connection con = Conexao.getConexao();
 		PreparedStatement stmt = null;
+		PreparedStatement stmtMedico = null;
+		PreparedStatement stmtEnfermeiro = null;
+		PreparedStatement stmtAux = null;
 
 		try {
 			con.setAutoCommit(false);
@@ -22,15 +28,62 @@ public class PessoaDAO{
 			stmt.setString(3, pTMP.getSexo());
 			stmt.setString(4, pTMP.getTelefone());
 			stmt.setDate(5, new Date(pTMP.getDtNascimento().getTime()));
-			
+
 			int rowAf = stmt.executeUpdate();
 			if (rowAf == 1) {
 				con.commit();
-				return true;
+				// return true;
 			} else {
 				con.rollback();
 				System.out.println("Falha ao inserir pessoa");
 				return false;
+			}
+
+			if (pTMP.getTipo() == 1) {
+				Medico m = (Medico) pTMP;
+				stmtMedico = con.prepareStatement("insert into medico(cpf,especialidade) values (?,?)");
+				stmtMedico.setString(1, m.getCpf());
+				stmtMedico.setInt(2, m.getEspecialidade());
+
+				rowAf = stmtMedico.executeUpdate();
+				if (rowAf == 1) {
+					con.commit();
+					return true;
+				} else {
+					con.rollback();
+					System.out.println("Falha ao inserir pessoa");
+					return false;
+				}
+			} else if (pTMP.getTipo() == 2) {
+				Enfermeiro e = (Enfermeiro) pTMP;
+				stmtEnfermeiro = con.prepareStatement("insert into enfermeiro (cpf,carga_horaria) values (?,?)");
+				stmtEnfermeiro.setString(1, e.getCpf());
+				stmtEnfermeiro.setInt(2, e.getCargaHoraria());
+
+				rowAf = stmtEnfermeiro.executeUpdate();
+				if (rowAf == 1) {
+					con.commit();
+					return true;
+				} else {
+					con.rollback();
+					System.out.println("Falha ao inserir pessoa");
+					return false;
+				}
+			} else {
+				AuxiliarAdministrativo a = (AuxiliarAdministrativo) pTMP;
+				stmtAux = con.prepareStatement("insert into auxAdm (cpf,salario) values (?,?)");
+				stmtAux.setString(1, a.getCpf());
+				stmtAux.setDouble(2, a.getSalario());
+
+				rowAf = stmtAux.executeUpdate();
+				if (rowAf == 1) {
+					con.commit();
+					return true;
+				} else {
+					con.rollback();
+					System.out.println("Falha ao inserir pessoa");
+					return false;
+				}
 			}
 
 		} catch (Exception e) {
@@ -44,6 +97,13 @@ public class PessoaDAO{
 			}
 		} finally {
 			try {
+				if (pTMP.getTipo() == 1) {
+					stmtMedico.close();
+				} else if (pTMP.getTipo() == 2) {
+					stmtEnfermeiro.close();
+				} else {
+					stmtAux.close();
+				}
 				stmt.close();
 				con.close();
 			} catch (SQLException e) {
@@ -145,15 +205,19 @@ public class PessoaDAO{
 		ResultSet rs = null;
 
 		try {
-			stmt = con.prepareStatement("select nome,dt_nasc from pessoa");
+			stmt = con.prepareStatement("select cpf,nome,sexo,telefone,dt_nascimento from pessoa");
 			rs = stmt.executeQuery();
 
 			while (rs.next()) {
 				Pessoa pTMP = new Pessoa();
+				pTMP.setCpf(rs.getString("cpf"));
 				pTMP.setNome(rs.getString("nome"));
-				pTMP.setDtNascimento(rs.getDate("dt_nasct"));
+				pTMP.setSexo(rs.getString("sexo"));
+				pTMP.setTelefone(rs.getString("telefone"));
+				pTMP.setDtNascimento(rs.getDate("dt_nascimento"));
 				alPessoa.add(pTMP);
 			}
+
 		} catch (SQLException e) {
 			System.out.println("Nao foi possivel recuperar lista de pessoas!!!");
 		} finally {
