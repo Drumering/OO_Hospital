55c6ab4eb43c15a97bec850f94a072d5695f8a66
opet <opet@RB-LAB207A-003.Academico.edu>
2019-05-27 22:11:49 -0300

    update listaEspecialidade

--- src/br/com/opet/controller/ControllerEspecialidade.java
+++ src/br/com/opet/controller/ControllerEspecialidade.java
@@ -3,14 +3,12 @@ package br.com.opet.controller;
 import java.util.ArrayList;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 
 import br.com.opet.model.Especialidade;
 import br.com.opet.model.dao.EspecialidadeDAO;
 import br.com.opet.view.TelaEspecialidade;
 
 @ManagedBean
-@RequestScoped
 public class ControllerEspecialidade {
 	TelaEspecialidade te = new TelaEspecialidade();
 	EspecialidadeDAO eDAO = new EspecialidadeDAO();
@@ -21,24 +19,18 @@ public class ControllerEspecialidade {
 		return "/especialidades/listarEspecialidades.xhtml";
 	}
 
-	
-	/*private void showAtualizar() {
-		ArrayList<Integer> listaId = Listar();
-		int id = -1;
-		do {
-			id = te.showMenuRecuperar();
-		} while (!listaId.contains(id));
-		Especialidade descricao;
-		try {
-			descricao = te.showCadastrar();
-			if (eDAO.atualizarEspecialidade(id, descricao.getDescricao())) {
-				System.out.println("Descricao atualizada com Sucesso");
-			}
-		} catch (Exception e) {
-			System.err.println("falha ao atualizar!");
-		}
-
-	}*/
+	/*
+	 * private void showAtualizar() { ArrayList<Integer> listaId = new
+	 * ArrayList<Integer>(); for (Especialidade esp : Listar()) {
+	 * listaId.add(esp.getId()); } int id = -1; do { id = te.showMenuRecuperar(); }
+	 * while (!listaId.contains(id)); Especialidade descricao; try { descricao =
+	 * te.showCadastrar(); if (eDAO.atualizarEspecialidade(id,
+	 * descricao.getDescricao())) {
+	 * System.out.println("Descricao atualizada com Sucesso"); } } catch (Exception
+	 * e) { System.err.println("falha ao atualizar!"); }
+	 * 
+	 * }
+	 */
 
 	public ArrayList<Especialidade> Listar() {
 		EspecialidadeDAO eDAO = new EspecialidadeDAO();
